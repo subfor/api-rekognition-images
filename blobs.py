302194a3321788ed9aab52b2cb50bb8ffa9c2ab4
alex <alex.kudelko@gmail.com>
2022-01-31 21:57:43 +0200

    add env

--- blobs.py
+++ blobs.py
@@ -7,13 +7,15 @@ import requests
 import boto3
 from botocore.exceptions import ClientError
 from serialize_json import from_dynamodb_to_json
+
+
 dynamodb = boto3.resource('dynamodb')
 
 
 def create_presigned_url_upload(key: str):
     s3_client = boto3.client('s3')
     try:
-        response = s3_client.generate_presigned_url('put_object', Params={'Bucket': 'images-recognition234234zcz3',
+        response = s3_client.generate_presigned_url('put_object', Params={'Bucket': os.environ['IMAGES_BUCKET'],
                                                                           'Key': key}, ExpiresIn=120)
     except ClientError as e:
         logging.error(e)
@@ -45,16 +47,6 @@ def make_callback(event, context):
     action_db = event['Records'][0]['eventName']
     if action_db == "MODIFY":
         data = from_dynamodb_to_json(event['Records'][0]['dynamodb']['NewImage'])
-        # d = from_dynamodb_to_json(data)
         callback_url = data['callback_url']
         labels = data['labels']
         requests.post(callback_url, json={"data": labels})
-
-# def make_callback(event, context):
-#     action_db = event['Records'][0]['eventName']
-#     if action_db == "MODIFY":
-#         data = event['Records'][0]['dynamodb']['NewImage']
-#         d = data['labels']['L']
-#         callback_url = data['callback_url']['S']
-#         # labels = data['labels']['S']
-#         requests.post(callback_url, json={"data": d})
--- processBlob.py
+++ processBlob.py
@@ -9,7 +9,7 @@ def rekognizeImage(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     file_name = event['Records'][0]['s3']['object']['key']
-    rekognitionClient = boto3.client('rekognition', region_name='eu-west-1')
+    rekognitionClient = boto3.client('rekognition', region_name=os.environ['REGION_NAME'])
     response = rekognitionClient.detect_labels(Image={'S3Object': {'Bucket': bucket_name, 'Name': file_name}},
                                                MaxLabels=5)
     labels = [{"label": item['Name'],
